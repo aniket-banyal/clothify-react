ac96ebfc2bba819f3afadb9aed6a3d5550d9ef1f
dummy26 <dummyani26@gmail.com>
2022-08-28 16:07:04 +0530

    Add query key factory for categories
    Closes #26

--- src/hooks/api/useCategories.js
+++ src/hooks/api/useCategories.js
@@ -1,25 +1,29 @@
-import { useQuery } from "@tanstack/react-query";
-import api from "../../api";
-import { reactQueryConstants } from "../../constants";
+import { useQuery } from "@tanstack/react-query"
+import api from "../../api"
+import { reactQueryConstants } from "../../constants"
 
+export const categoryKeys = {
+    all: ["categories"],
+    lists: () => [...categoryKeys.all, "list"],
+    list: ({ gender = "" }) => {
+        return [...categoryKeys.lists(), { gender }]
+    },
+}
 
 const getCategories = async (gender) => {
-    if (!gender)
-        gender = ''
+    if (!gender) gender = ""
 
     const searchParams = new URLSearchParams({ gender })
 
-    const { data } = await api.get(
-        "/clothes/categories",
-        { params: searchParams }
-    )
+    const { data } = await api.get("/clothes/categories", {
+        params: searchParams,
+    })
     return data
 }
 
-
 export default function useCategories(gender) {
     return useQuery(
-        [`categories ${gender}`],
+        categoryKeys.list({ gender }),
         () => getCategories(gender),
         {
             suspense: true,
